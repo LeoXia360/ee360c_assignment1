3f097beba3dc369418e59944fee345cca8dddabd
Leo Xia <leoxia@wireless-10-145-115-61.public.utexas.edu>
2017-02-02 13:23:52 -0600

    added SearchThread

---
+++ hw1-template/q2/SearchThread.java
@@ -0,0 +1,27 @@
+//UT-EID=LX939
+
+
+import java.util.*;
+import java.util.concurrent.*;
+
+
+public class SearchThread implements Callable<Integer>{
+	private int x;
+	private int[] sub;
+	
+	public SearchThread(int x, int[] sub){
+		 this.x = x;
+		 this.sub = sub;
+	}
+	
+	public Integer call(){
+		for(int i = 0; i < sub.length; i++){
+			if(sub[i] == x){
+				return i;
+			}
+		}
+		//didn't find in this array
+		return -1;
+	}
+  
+}
